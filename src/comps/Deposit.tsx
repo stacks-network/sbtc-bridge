100b454bf877c2b059dd9deda6fc0b1bdb69d25b
janniks <janniks@users.noreply.github.com>
2025-02-21 00:43:15 +0800

    chore: remove import

--- src/comps/Deposit.tsx
+++ src/comps/Deposit.tsx
@@ -40,7 +40,6 @@ import { sendBTC } from "@/util/wallet-utils";
 import useMintCaps from "@/hooks/use-mint-caps";
 import { getAggregateKey } from "@/util/get-aggregate-key";
 import getBitcoinNetwork from "@/util/get-bitcoin-network";
-import { useAsignaConnect } from "@asigna/btc-connect";
 import { useQuery } from "@tanstack/react-query";
 import getBtcBalance from "@/actions/get-btc-balance";
 import { useDepositStatus } from "@/hooks/use-deposit-status";
